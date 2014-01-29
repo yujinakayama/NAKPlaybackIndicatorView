659d917acd2f5da137e506784acc3862225e7dad
Yuji Nakayama <nkymyj@gmail.com>
2014-01-29 21:33:06 +0900

    Add documentation comments for method

--- Classes/NAPlaybackIndicatorView.h
+++ Classes/NAPlaybackIndicatorView.h
@@ -10,9 +10,19 @@
 
 @interface NAPlaybackIndicatorView : UIView
 
+/**
+ Starts the oscillating animation of the bars.
+ */
 - (void)startAnimating;
+
+/**
+ Stop the oscillating animation of the bars.
+ */
 - (void)stopAnimating;
 
+/**
+ Whether the receiver is animating.
+ */
 @property (nonatomic, readonly, getter=isAnimating) BOOL animating;
 
 @end
