75a3bae43c0de775836f44818f8ce7899b6f4d27
Yuji Nakayama <nkymyj@gmail.com>
2014-02-02 20:03:19 +0900

    Improve appledoc comments

--- Classes/NAKPlaybackIndicatorView.h
+++ Classes/NAKPlaybackIndicatorView.h
@@ -12,16 +12,31 @@
  Values for the [state]([NAKPlaybackIndicatorView state]) property.
  */
 typedef NS_ENUM(NSInteger, NAKPlaybackIndicatorViewState) {
+    /**
+     Stopped state of an indicator view.
+     In this state, if an indicator's [hidesWhenStopped]([NAKPlaybackIndicatorView hidesWhenStopped]) is `YES`, the indicator becomes hidden.
+     Or if an indicator's [hidesWhenStopped]([NAKPlaybackIndicatorView hidesWhenStopped]) is `NO`, the indicator shows idle bars.
+     */
     NAKPlaybackIndicatorViewStateStopped = 0,
+
+    /**
+     Playing state of an indicator view.
+     In this state, an indicator shows oscillatory animated bars.
+     */
     NAKPlaybackIndicatorViewStatePlaying,
+
+    /**
+     Paused state of an indicator view.
+     In this state, an indicator shows idle bars.
+     */
     NAKPlaybackIndicatorViewStatePaused
 };
 
 /**
  `NAKPlaybackIndicatorView` is a view that mimics the music playback indicator in the Music.app on iOS 7.
 
- The view has three vertical bars and they oscillate randomly when it’s playing state.
- The color of the bars can be changed by setting `tintColor` property (`UIView`) of the receiver or its ancestor views.
+ It has three vertical bars and they oscillate randomly.
+ The color of the bars can be changed by setting `tintColor` property (`UIView`) of the receiver or its ancestor view.
 
  Out of the box it works well with Auto Layout system as it provides sensible layout information
  such as intrinsic content size, baseline, and priorities of content hugging / compression resistence.
@@ -32,15 +47,15 @@ typedef NS_ENUM(NSInteger, NAKPlaybackIndicatorViewState) {
 /**
  The current state of the receiver.
 
- You can control the receiver’s appearance and behavior by setting this property.
+ You can control the receiver's appearance and behavior by setting this property.
 
  - `NAKPlaybackIndicatorViewStateStopped`:
-   - If hidesWhenStopped is YES, the receiver is hidden.
-   - If hidesWhenStopped is NO, the receiver shows idle bars (same as `NAKPlaybackIndicatorViewStatePaused`).
+   - If hidesWhenStopped is `YES`, the receiver becomes hidden.
+   - If hidesWhenStopped is `NO`, the receiver shows idle bars (same as `NAKPlaybackIndicatorViewStatePaused`).
  - `NAKPlaybackIndicatorViewStatePlaying`: The receiver shows oscillatory animated bars.
  - `NAKPlaybackIndicatorViewStatePaused`: The receiver shows idle bars.
 
- The initial value of this property is `NAKPlaybackIndicatorViewStateStopped`.
+ The initial value is `NAKPlaybackIndicatorViewStateStopped`.
  */
 @property (nonatomic, assign) NAKPlaybackIndicatorViewState state;
 
@@ -51,7 +66,7 @@ typedef NS_ENUM(NSInteger, NAKPlaybackIndicatorViewState) {
  If the value of this property is `YES` (the default),
  the receiver sets its `hidden` property (`UIView`) to `YES`
  when receiver’s state is `NAKPlaybackIndicatorViewStateStopped`.
- If the value of this property is `NO`, the receiver is shown when it’s stopped state.
+ If the value is `NO`, the receiver is shown even when it's stopped.
 
  Note that by setting state `NAKPlaybackIndicatorViewStatePlaying` or `NAKPlaybackIndicatorViewStatePaused`
  the receiver will be shown automatically.
